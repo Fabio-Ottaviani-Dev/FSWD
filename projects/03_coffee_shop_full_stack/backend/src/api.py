c47151f9bda1ff9e5cd9a820d4ad0f29a508d1fd
Fabio-Ottaviani-Dev <F0Dev>
2019-11-22 14:44:18 -0800

    implemented Auth0 in the backend.

--- projects/03_coffee_shop_full_stack/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/backend/src/api.py
@@ -29,13 +29,48 @@ def reset_db():
         'message': 'db_drop_and_create_all @DONE!'
     }), 200
 
-## ROUTES
-@app.route('/')
-def index():
+# ----------------------------------------------------------------------------
+# Create >> drink
+# ----------------------------------------------------------------------------
+# @TODO implement endpoint
+#     POST /drinks
+#         1. it should create a new row in the drinks table
+#         2. it should require the 'post:drinks' permission
+#         3. it should contain the drink.long() data representation
+#         4. returns status code 200 and json {"success": True, "drinks": drink}
+#            where drink an array containing only the newly created drink
+#            or appropriate status code indicating reason for failure
+# @NOTES
+    # recipe field data structure:
+    # [{"color": "string", "name":"string", "parts":"number"}]
+# @DONE
+# ----------------------------------------------------------------------------
+
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(payload):
+
+    title    = request.json.get('title', None)
+    recipe   = json.dumps(request.json.get('recipe', None))
+
+    if title is None or recipe is None:
+        abort(400)
+
+    drink = Drink(
+        title   = title,
+        recipe  = recipe
+    )
+
+    try:
+        drink.insert()
+    except:
+        abort(422)
+
     return jsonify({
-        'success': True,
-        'message': 'What up Dude.. do you want a coffee.!?'
-    }), 200
+        'success':  True,
+        'drink':    drink.long()
+    }), 201
+
 # ----------------------------------------------------------------------------
 # Read >> drinks list short
 # ----------------------------------------------------------------------------
@@ -62,6 +97,7 @@ def get_drinks():
         'success':  True,
         'drinks':   result
     }), 200
+
 # ----------------------------------------------------------------------------
 # Read >> drinks list detail
 # ----------------------------------------------------------------------------
@@ -72,10 +108,12 @@ def get_drinks():
 #       3. returns status code 200 and json {"success": True, "drinks": drinks}
 #          where drinks is the list of drinks or appropriate status code
 #          indicating reason for failure.
+# @DONE
 # ----------------------------------------------------------------------------
 
 @app.route('/drinks-detail', methods=['GET'])
-def get_drinks_detail():
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
 
     drinks          = Drink.query.order_by(Drink.title).all()
     result          = [drink.long() for drink in drinks]
@@ -90,46 +128,6 @@ def get_drinks_detail():
     }), 200
 
 # ----------------------------------------------------------------------------
-# Create >> drink
-# ----------------------------------------------------------------------------
-# @TODO implement endpoint
-#     POST /drinks
-#         1. it should create a new row in the drinks table
-#         2. it should require the 'post:drinks' permission
-#         3. it should contain the drink.long() data representation
-#         4. returns status code 200 and json {"success": True, "drinks": drink}
-#            where drink an array containing only the newly created drink
-#            or appropriate status code indicating reason for failure
-# @NOTES
-    # recipe field data structure:
-    # [{"color": "string", "name":"string", "parts":"number"}]
-# ----------------------------------------------------------------------------
-
-@app.route('/drinks', methods=['POST'])
-def create_drink():
-
-    title    = request.json.get('title', None)
-    recipe   = json.dumps(request.json.get('recipe', None))
-
-    if title is None or recipe is None:
-        abort(400)
-
-    drink = Drink(
-        title   = title,
-        recipe  = recipe
-    )
-
-    try:
-        drink.insert()
-    except:
-        abort(422)
-
-    return jsonify({
-        'success':  True,
-        'drink':    drink.long()
-    }), 201
-
-# ----------------------------------------------------------------------------
 # Update >> drink
 # ----------------------------------------------------------------------------
 # @TODO implement endpoint
@@ -142,10 +140,12 @@ def create_drink():
 #            returns status code 200 and json {"success": True, "drinks": drink}
 #            where drink an array containing only the updated drink
 #            or appropriate status code indicating reason for failure
+# @DONE
 # ----------------------------------------------------------------------------
 
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
-def update_drink(drink_id):
+@requires_auth('patch:drinks')
+def update_drink(payload, drink_id):
 
     drink = Drink.query.get_or_404(drink_id)
 
@@ -180,10 +180,12 @@ def update_drink(drink_id):
 #         4. returns status code 200 and json {"success": True, "delete": id}
 #            where id is the id of the deleted record
 #            or appropriate status code indicating reason for failure
+# @DONE
 # ----------------------------------------------------------------------------
 
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
-def delete_drink(drink_id):
+@requires_auth('delete:drinks')
+def delete_drink(payload, drink_id):
 
     drink = Drink.query.get_or_404(drink_id)
 
@@ -201,6 +203,6 @@ def delete_drink(drink_id):
 # Error Handling
 # @TODO
 #   1. implement error handlers using the @app.errorhandler(error) decorator
-# @DONE
 #   2. implement error handler for AuthError
+# @DONE
 # ----------------------------------------------------------------------------
